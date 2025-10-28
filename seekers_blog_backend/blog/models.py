7101b76c9c2f9a0d78ff7c85350f644b0d80d254
Zarry Weeden <zarryochola@gmail.com>
2025-10-28 14:00:13 +0300

    Cloudinary retouch

--- seekers_blog_backend/blog/models.py
+++ seekers_blog_backend/blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='category_images/', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
@@ -16,7 +17,7 @@ class Category(models.Model):
 class Author(models.Model):  # NEW MODEL
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=100)
-    profile_image = models.ImageField(upload_to='author_images/', blank=True, null=True)
+    profile_image = CloudinaryField('image', blank=True, null=True) 
     bio = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
@@ -32,7 +33,7 @@ class BlogPost(models.Model):
     excerpt = models.TextField(max_length=300, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    featured_image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
+    featured_image = CloudinaryField('image', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=False)
--- seekers_blog_backend/seekers_api/settings.py
+++ seekers_blog_backend/seekers_api/settings.py
@@ -154,12 +154,9 @@ CORS_ALLOW_ALL_ORIGINS = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
-import os
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
@@ -168,6 +165,13 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
+
 
 
 
