78fdfe0abf751ab9f3443baa65c8392a9ccbb576
Zarry Weeden <zarryochola@gmail.com>
2025-10-28 13:32:32 +0300

    retouch

--- seekers_blog_backend/seekers_api/settings.py
+++ seekers_blog_backend/seekers_api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g3*%enjk3tfx=!^0fwqml2&y8r)+pon_i2puf9w9_b$pe5-ao@'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key-for-dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
