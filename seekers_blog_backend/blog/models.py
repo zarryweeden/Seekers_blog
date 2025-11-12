d1f4f934aee0b51dd8f209f99ca06a3cb4b91b66
Zarry Weeden <zarryochola@gmail.com>
2025-11-12 16:29:29 +0300

    Fixed Cloudinary Errors

--- seekers_blog_backend/blog/models.py
+++ seekers_blog_backend/blog/models.py
@@ -1,29 +1,25 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image = CloudinaryField('image', blank=True, null=True)
+    image = models.ImageField(upload_to='categories/', blank=True, null=True)  
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
 
 
-class Author(models.Model):  # NEW MODEL
+class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=100)
-    profile_image = CloudinaryField('image', blank=True, null=True) 
+    profile_image = models.ImageField(upload_to='authors/', blank=True, null=True)  
     bio = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.display_name
-
 
 
 class BlogPost(models.Model):
@@ -33,13 +29,13 @@ class BlogPost(models.Model):
     excerpt = models.TextField(max_length=300, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    featured_image = CloudinaryField('image', blank=True, null=True)
+    featured_image = models.ImageField(upload_to='blog/', blank=True, null=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published = models.BooleanField(default=False)
     views = models.IntegerField(default=0)
     featured = models.BooleanField(default=False)
-    featured_order= models.IntegerField(default=0)
+    featured_order = models.IntegerField(default=0)
     
     def __str__(self):
         return self.title
@@ -48,4 +44,3 @@ class BlogPost(models.Model):
         if not self.slug:
             self.slug = self.title.replace(' ', '-').lower()
         super().save(*args, **kwargs)
-    
