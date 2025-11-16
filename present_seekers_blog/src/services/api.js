a174c56f06a722f058d7954356c3bee2c441b778
Zarry Weeden <zarryochola@gmail.com>
2025-11-16 14:29:49 +0300

    Remove Cloudinary migrations and create fresh initial migration

--- present_seekers_blog/src/services/api.js
+++ present_seekers_blog/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://seekers-blog.onrender.com/api/blog';
-
+// For local development - use your local Django server
+const API_BASE_URL = 'http://127.0.0.1:8000/api/blog';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -11,25 +11,12 @@ const api = axios.create({
 });
 
 export const blogAPI = {
-  // Get all blog posts
   getPosts: () => api.get('/posts/'),
-  
-  // Get posts by category
   getPostsByCategory: (category) => api.get(`/posts/?category=${category}`),
-  
-  // Get single post by ID
   getPost: (id) => api.get(`/posts/${id}/`),
-  
-  // Get all categories
   getCategories: () => api.get('/categories/'),
-  
-  // Get featured posts
   getFeaturedPosts: () => api.get('/posts/featured/'),
-  
-  // Increment views
   incrementViews: (id) => api.post(`/posts/${id}/increment_views/`),
 };
-
-
 
 export default api;
